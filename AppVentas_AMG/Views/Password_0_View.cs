de281905f0d6fdb53c2970cedfba768ed50a1874
Antonio Martinez Gonzalez <amartinez@itesa.edu.mx>
2024-02-01 13:48:08 -0600

    Versión_01: Se creó la arquitectura del proyecto

---
+++ AppVentas_AMG/Views/Password_0_View.cs
@@ -0,0 +1,15 @@
+namespace AppVentas_AMG
+{
+    public partial class Password_0_View : Form
+    {
+        public Password_0_View()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
